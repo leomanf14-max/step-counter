19079a5fe3c6c66da2558c3c4a887090350d3707
leomanf14-max <leomanf14@gmail.com>
2025-09-16 15:41:09 +0200

    Aggiorna pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,36 @@ input.onGesture(Gesture.ScreenDown, function () {
     passi += 1
 })
 let passi = 0
+basic.showString("MICRO:BIT")
+for (let index = 0; index < 4; index++) {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+record.startRecording(record.BlockingState.Blocking)
 basic.forever(function () {
     basic.showNumber(passi)
+    music.play(music.createSoundExpression(
+    WaveShape.Sawtooth,
+    1,
+    5000,
+    76,
+    255,
+    4003,
+    SoundExpressionEffect.Tremolo,
+    InterpolationCurve.Logarithmic
+    ), music.PlaybackMode.InBackground)
     if (input.logoIsPressed()) {
         basic.showLeds(`
             # . . . #
@@ -12,5 +40,6 @@ basic.forever(function () {
             . . # . .
             . . # . .
             `)
+        record.playAudio(record.BlockingState.Blocking)
     }
 })
